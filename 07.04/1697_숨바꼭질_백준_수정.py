c0e62aeb16bf973e66836f5bf7904ebb3f573f6e
cksghkd123 <cksghkd1234@naver.com>
2021-07-04 22:36:37 +0900

    숨바꼭질 런타임에러,메모리초과 해결

--- 07.04/1697_숨바꼭질_백준_수정.py
+++ 07.04/1697_숨바꼭질_백준_수정.py
@@ -9,10 +9,16 @@ def case(N):
     return plus1,minus1,double
 
 def HNS(N,K):
+    if N >= K:
+        count = N - K
+        return count
+    
+    visited = [0 for _ in range(100000*2 + 1)]
     deq = collections.deque()
     onetime = collections.deque()
     deq.append(N)
     count = 0
+    visited[N] = 1
 
     while deq:
         count += 1
@@ -23,10 +29,17 @@ def HNS(N,K):
         while onetime:
             N = onetime.popleft()
             n1, n2, n3 = case(N)
+            n = [n1,n2,n3]
+
             if n1 == K or n2 == K or n3 == K:
                 return count
+            
+            for w in n:
+                if 0 <= w <= 100000:
+                    if visited[w] == 0:
+                        deq.append(w)
+                        visited[w] = 1
 
-            deq.extend([n1,n2,n3])
 
 
 N, K = map(int,input().split())

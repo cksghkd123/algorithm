5d444c98a9386f19905e33223fe8d9e82cd08620
cksghkd123 <cksghkd1234@naver.com>
2021-07-07 01:47:06 +0900

    N-Queen해결

--- 07.06/9663_N-Queen_백준.py
+++ 07.06/9663_N-Queen_백준.py
@@ -1,13 +1,19 @@
+from pprint import pprint
 def queen(row,col):
-    if len(que) == N:
-        global count
-        count += 1
-        print(count)
-        return
     
     chessboard[row][col] = 1
     que.append((row,col))
     rque.append(row)
+
+
+    if len(que) == N:
+        global count
+        count += 1
+        que.pop()
+        rque.pop()
+        return
+
+    
     
     for w in range(N):
         
@@ -15,18 +21,22 @@ def queen(row,col):
             continue
         nr = w
         break
-
+        
     for nc in range(N):
         BREAK = False
+        Button = False
         for w in range(len(que)):
-
-            if nc == que[w][1]-abs(que[w][0]-nr) or nc == que[w][1]+abs(que[w][0]-nr) or nc == que[w][1]:
-                
+            if nc == que[w][1]-abs(que[w][0]-nr) or nc == que[w][1]+abs(que[w][0]-nr) or nc == que[w][1]: 
                 BREAK = True
                 break
         if BREAK == True:
+            if nc == N-1:
+                rque.pop()
+                que.pop()
+                return
             continue
         queen(nr,nc)
+            
     
     rque.pop()
     que.pop()

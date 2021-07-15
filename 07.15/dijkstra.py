bda011c5bd461489cb8947cf11e24d11b27a9409
cksghkd123 <cksghkd1234@naver.com>
2021-07-15 16:46:11 +0900

    크루스칼 다익스트라

---
+++ 07.15/dijkstra.py
@@ -0,0 +1,58 @@
+# 입력 - 마지막은 시작정점
+# 6 11
+# 1 2 3
+# 1 3 3
+# 1 4 3
+# 2 3 2
+# 4 3 5
+# 4 5 2
+# 5 4 1
+# 3 5 1
+# 6 3 1
+# 3 6 4
+# 5 6 2
+# 1
+# 정점, 간선의 수
+V, E = map(int, input().split())
+
+# 인접 리스트 초기화
+maplist = [[] for i in range(V+1)]
+
+# 간선 정보를 인접리스트에 저장
+for _ in range(E):
+    s, e, l = map(int, input().split())
+    maplist[s].append((e, l))
+
+# 시작 정점 입력
+start = int(input())
+
+# distance 리스트 초기화
+distance = [float('inf') for _ in range(V+1)]
+distance[start] = 0
+
+# selected 리스트 초기화
+selected = [0 for i in range(V+1)]
+selected[0] = 1
+
+# 아래 과정을 정점 수 만큼 반복
+for _ in range(1, V+1):
+    # 방문하지 않았으면서, 알려진 거리가 가장 짧은 노드를
+    # min_node에 저장
+    min_value = float('inf')
+    min_node = 0
+    for i in range(1, V+1):
+        if distance[i] < min_value and not selected[i]:
+            min_value = distance[i]
+            min_node = i
+    
+    # min_node 방문표시
+    selected[min_node] = 1
+    
+    # min_node 에서 갈 수 있는 각 정점들 중
+    # 알려진 거리 (distance[next_node]) 와
+    # 새로운 거리 (distance[min_node] + l) 중 작은 값으로 갱신
+    for next_node, l in maplist[min_node]:
+        distance[next_node] = min(distance[next_node], distance[min_node] + l)
+
+# 거리정보 출력
+print(distance)
---
+++ 07.15/kruskal.py
@@ -0,0 +1,40 @@
+#{정점1} {정점2} {비용}
+Elist = [(1, 2, 3), (1, 4, 6), (2, 4, 5), (2, 5, 4), (4, 5, 2), (3, 4, 2), (5, 8, 4), (5, 7, 2), (4, 7, 1), (3, 6, 3), (6, 7, 4), (7, 8, 6)]
+
+#간선의 비용을 오름차순으로 정렬한다.
+Elist.sort(key=lambda x:x[2])
+
+#union & find 를 정의한다.
+#정점의 개수
+V = 8
+parent = [i for i in range(V+1)]
+
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x,y):
+    fx = find(x)
+    fy = find(y)
+    if fx != fy:
+        parent[fy] = fx
+    
+#이제 정렬된 간선을 하나씩 검사한다. 만약 해당 차례의 간선이 이미 만들어진 MST에 속해있다면 
+#(find(v1) == find(v2) 인 경우) 아무것도 하지 않고, 속해있지 않다면 union을 통해 병합한다.
+total_cost = 0
+mst_info = []
+for i in range(len(Elist)):
+    v1, v2, cost = Elist[i]
+    if find(v1) != find(v2):
+        union(v1, v2)
+        mst_info.append(f"[{v1}-{v2}]")
+        total_cost += cost
+
+print(total_cost)
+for info in mst_info:
+    print(info)
+
+#시간복잡도는 정렬 알고리즘에 따라 달라지는데, O(nlogn)의 정렬 알고리즘을 사용하면 
+#최종 시간복잡도는 O(nlogn + n) = O(nlogn)이 된다.

0f912d3370bf7f1940a0389078dc65c5ec8ef27a
cksghkd123 <cksghkd1234@naver.com>
2021-07-30 16:36:59 +0900

    브루트포스 dfs

--- 백준부수기/15684_사다리조작_백준.py
+++ 백준부수기/15684_사다리조작_백준.py
@@ -1,77 +1,48 @@
-import collections
-import heapq
+from pprint import pprint
 
 
-def get_start(n, m, visited):
-    for row in range(m):
-        for col in range(n-1):
-            if visited(row, col) == False:
-                return (row, col)
-    else:
-        return -1
-        
+def check_ladder():
+    for col in range(n):
+        where = col
+        for row in range(h):
+            where += ladder_list[row][where]
 
-def play_ladder(init_lines, ss):
-    deq = collections.deque()
-    deq.append((init_lines, ss))
-
-    while deq:
-        lines, ss = deq.popleft()
-        success = ladder_check(lines)
-
-        if success:
-            break
-        
-        for row in range(ss[0], m):
-            for col in range(n-1):
-                if row == ss[0] and col <ss[1]:
-                    continue
-                deq.append(init_lines + [(row, col)], )
-
-        print(lines)
-
-
-
-def ladder_check(lines):
-    result = [i for i in range(n)]
-    print(lines)
-    for k in range(len(lines)):
-        print(result)
-        row, col = heapq.heappop(lines)
-        result[col], result[col+1] = result[col+1], result[col]
-    print(result)
-
-    for col in range(len(result)):
-        if result[col] != col:
+        if where != col:
             return False
     else:
         return True
 
-
+def play_ladder(row, col, result):
+    global answer
+    possible = check_ladder()
+    if possible == True:
+        answer = min(answer, result)
+        return 
+    
+    if result == 3:
+        return
+    
+    for nr in range(row, h):
+        for nc in range(col, n-1):
+            if ladder_list[nr][nc] == 0 and ladder_list[nr][nc+1] == 0:
+                ladder_list[nr][nc] = 1
+                ladder_list[nr][nc+1] = -1
+                play_ladder(nr, nc, result+1)
+                ladder_list[nr][nc]= 0
+                ladder_list[nr][nc+1] = 0
+        col = 0
+    
 n, m, h = map(int,input().split())
-ladder_list = []
+ladder_list = [[0]*n for _ in range(h)]
 
 for _ in range(m):
-    row, col = map(lambda x: int(x)-1 ,input().split())
-    ladder_list.append((row, col))
+    row, col = map(lambda x: int(x)-1,input().split())
+    ladder_list[row][col] = 1
+    ladder_list[row][col+1] = -1
 
-ladder_list.sort()
-
-visited = [[False]*n for _ in range(m)]
-
-for row, col in ladder_list:
-    if 0 < col < m:
-        visited[row][col-1] = True
-    visited[row][col] = True
-    visited[row][col+1] = True
-
-start = get_start(n, m, visited)
-
-print(start)
-if start == -1:
-    result = ladder_check(ladder_list)
-
+answer = 4
+play_ladder(0, 0, 0)
+if answer == 4:
+    print(-1)
 else:
-    result = play_ladder(ladder_list, start)
-
-print(result)
+    print(answer)

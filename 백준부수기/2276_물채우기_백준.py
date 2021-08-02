fe3d685698779f3420b5b8650bcde564d6c76446
cksghkd123 <cksghkd1234@naver.com>
2021-08-02 14:18:54 +0900

    어렵구만

--- 백준부수기/2276_물채우기_백준.py
+++ 백준부수기/2276_물채우기_백준.py
@@ -1,21 +1,29 @@
+def find(row, col):
+    if root[row][col] = (row, col)
+
+
 dr = [0 ,0, 1, -1]
 dc = [1, -1, 0, 0]
 def pouring(row, col):
-
+    temp = 0
     for w in range(4):
         nr = row + dr[w]
         nc = col + dc[w]
+        if 0 <= nr < m and 0 <= nc < n:
+            if bottle[row][col] > bottle[nr][nc]:
+                
+
+        else:
+            
         
         
 
 m, n = map(int,input().split())
 bottle = [list(map(int,input().split())) for _ in range(m)]
-water = [[0]*m for _ in range(n)]
+water = [[0 for i in range(n)] for _ in range(m)]
 
-# result = 0
-# for row in range(1,m-1):
-#     for col in range(1,n-1):
-#         if visited[row][col] == False:
-#             result += pouring(row, col)
-for i in range(m):
-    print(bottle[i])
+result = 0
+for row in range(m):
+    for col in range(n):
+        pouring(row, col)
+
--- 백준부수기/ex.py
+++ 백준부수기/ex.py
@@ -1,6 +1,8 @@
-b , c = 1, 2
-a = [b, c]
-print(a)
-b, c = 5, 6
-print(a)
+# directions = ( [(1,0), (-1,0), (0,1), (0,-1)] )
+# directions = ( ((1,0), (-1,0), (0,1), (0,-1)) )
+directions = [ ((1,0), (-1,0), (0,1), (0,-1)) ] #내가 원하는 결과는 이거뿐임
 
+
+for ds in directions:
+
+	print(ds)

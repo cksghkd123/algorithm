5dc954b028bfbb0fbcdfdc3cf272d7958fc97c8f
cksghkd123 <cksghkd1234@naver.com>
2021-08-08 23:25:44 +0900

    백크레킹 구현

--- 백준부수기/17825_주사위윷놀이_백준.py
+++ 백준부수기/17825_주사위윷놀이_백준.py
@@ -1,6 +1,6 @@
 def go_yut(where, dice):
-    if where == 36:
-        return 36, 0
+    if where == 35:
+        return 35, 0
     
     if dice != 0:
         return go_yut(yut_table[where][2], dice-1)
@@ -17,14 +17,23 @@ def go_yut(where, dice):
     
 
 def play_yut(member, time, total_score):
-    if time == 10:
+    if time == len(dice_sequence):
         global result
         result = max(result, total_score)
         return
+
     for m in range(len(member)):
-        if member[m] < 36:
+        if member[m] < 35:
             nextwhere, score = go_yut(member[m], dice_sequence[time])
-            if yut_table[3] == False:
+            if nextwhere == 35:
+                yut_table[member[m]][3] = False
+                temp = member[m]
+                member[m] = nextwhere
+                play_yut(member, time+1, total_score+score)
+                member[m] = temp
+                yut_table[member[m]][3] = True
+                
+            elif yut_table[nextwhere][3] == False:
                 yut_table[member[m]][3] = False
                 yut_table[nextwhere][3] = True
                 temp = member[m]
@@ -33,18 +42,19 @@ def play_yut(member, time, total_score):
                 member[m] = temp
                 yut_table[nextwhere][3] = False
                 yut_table[member[m]][3] = True
+
     if len(member) < 4:
         nextwhere, score = go_yut(0, dice_sequence[time])
-        yut_table[nextwhere][3] = True
-        play_yut(member+[nextwhere], time+1, total_score+score)
-        yut_table[nextwhere][3] = False
+        if yut_table[nextwhere][3] == False:
+            yut_table[nextwhere][3] = True
+            play_yut(member+[nextwhere], time+1, total_score+score)
+            yut_table[nextwhere][3] = False
         
 
 dice_sequence = list(map(int,input().split()))
-yut_table = [[0,0,1]] + [[i,2*i,i+1,False] for i in range(1,21)] + [[i+21, 3*i+10, i+22, False] for i in range(4)] + [[i+25, 2*i+20, i+26, False] for i in range(3)] + [[28, 30, 29, False]] + [[i+29, 28-i, i+29, False] for i in range(4)] + [[i+33, 5*i+30, i+34, False] for i in range(3)] + [[36, []]]
+yut_table = [[0,0,1]] + [[i,2*i,i+1,False] for i in range(1,20)] + [[20,40,35,False]] + [[i+21, 3*i+10, i+22, False] for i in range(4)] + [[i+25, 2*i+20, i+26, False] for i in range(3)] + [[28, 30, 29, False]] + [[i+29, 28-i, i+30, False] for i in range(4)] + [[33, 30, 34, False]] + [[34, 35, 20, False]] + [[35, []]]
 yut_table[24][2] = 32
 yut_table[27][2] = 32
 result = 0
-print(yut_table)
 play_yut([], 0, 0)
 print(result)

78a601718fb27cdc58b4ad00f71dcd1d2c68ae24
cksghkd123 <cksghkd1234@naver.com>
2021-08-12 23:04:13 +0900

    .

--- 백준부수기/1.py
+++ 백준부수기/1.py
@@ -1,5 +1,22 @@
-a = (3,5)
-b = (3,7)
+def solution(board, r, c):
+    not_clear = [True for _ in range(7)]
+    for row in range(4):
+        for col in range(4):
+            if board[row][col] != 0 and not_clear[board[row][col]] == True:
+                not_clear[board[row][col]] = False
+    
+    print(not_clear)
+    
+    answer = 0
+    return answer
 
-print(max(a,b))
-print(max(b,a))
+
+
+a = [
+    [1,0,0,3],
+    [2,0,0,0],
+    [0,0,0,2],
+    [3,0,1,0]]
+b = 1
+c = 0
+print(solution(a,b,c))

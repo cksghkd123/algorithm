fdf5059c58a6b1e6f3e64118b0718adbbecc531b
cksghkd123 <cksghkd1234@naver.com>
2021-08-14 23:13:04 +0900

    .

---
+++ 백준부수기/ex2.py
@@ -0,0 +1,12 @@
+import csv
+
+def solution(csv_string, keyword):
+    answer = 0
+    return answer
+
+q = """
+조직 ID,조직명,상위 조직 ID,소속 팀원 수\n1,토스팀,,1\n2,인터널 트라이브,1,1\n3,인터널 매니저 팀,2,7\n4,비바 플랫폼 팀,2,14\n5,아웃터널 트라이브,1,2\n6,가이드 팀,5,4\n7,피트아웃 사일로,5,11
+"""
+rdr = csv.reader(q, delimiter=',')
+for line in rdr:
+    print(line)

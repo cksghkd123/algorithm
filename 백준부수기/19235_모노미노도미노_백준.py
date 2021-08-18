ffadf06000ecff0c2be9663d9cc65e5de65f46c9
cksghkd123 <cksghkd1234@naver.com>
2021-08-18 22:30:26 +0900

    .구현.

--- 백준부수기/19235_모노미노도미노_백준.py
+++ 백준부수기/19235_모노미노도미노_백준.py
@@ -1,3 +1,56 @@
+from pprint import pprint
+
+def g_rain_it_one(row, col):
+    green_block[row][col] = 0
+    row += 1
+    while row < 6:
+        if green_block[row][col] != 0:
+            green_block[row-1][col] = 1
+            return
+        row += 1
+    
+    green_block[5][col] = 1
+
+def g_rain_it_two(row, col):
+    green_block[row][col] = 0
+    green_block[row][col+1] = 0
+    row += 1
+    while row < 6:
+        if green_block[row][col] != 0 or green_block[row][col+1] != 0:
+            green_block[row-1][col] = 2
+            green_block[row-1][col+1] = 2
+            return
+        row += 1
+    
+    green_block[5][col] = 2
+    green_block[5][col+1] = 2
+
+def b_rain_it_one(row, col):
+    blue_block[row][col] = 0
+    row += 1
+    while row < 6:
+        if blue_block[row][col] != 0:
+            blue_block[row-1][col] = 1
+            return
+        row += 1
+    
+    blue_block[5][col] = 1
+
+def b_rain_it_two(row, col):
+    blue_block[row][col] = 0
+    blue_block[row][col+1] = 0
+    row += 1
+    while row < 6:
+        if blue_block[row][col] != 0 or blue_block[row][col+1] != 0:
+            blue_block[row-1][col] = 2
+            blue_block[row-1][col+1] = 2
+            return
+        row += 1
+    
+    blue_block[5][col] = 2
+    blue_block[5][col+1] = 2
+    
+        
 n = int(input())
 
 green_block = [[0 for _ in range(4)] for _ in range(6)]
@@ -5,6 +58,7 @@ blue_block = [[0 for _ in range(4)] for _ in range(6)]
 point = 0
 
 for _ in range(n):
+    # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     t, r, c = map(int,input().split())
 
     #블럭 쌓기
@@ -13,22 +67,32 @@ for _ in range(n):
             if green_block[i][c] != 0:
                 green_block[i-1][c] = 1
                 break
+        else:
+            green_block[5][c] = 1
         for i in range(2,6):
             if blue_block[i][r] != 0:
                 blue_block[i-1][r] = 1
                 break
+        else:
+            blue_block[5][r] = 1
 
     elif t == 2:
         for i in range(2,6):
             if green_block[i][c] != 0 or green_block[i][c+1] != 0:
-                green_block[i-1][c] = 1
-                green_block[i-1][c+1] = 1
+                green_block[i-1][c] = 2
+                green_block[i-1][c+1] = 2
                 break
+        else:
+            green_block[5][c] = 2
+            green_block[5][c+1] = 2
         for i in range(2,6):
             if blue_block[i][r] != 0:
                 blue_block[i-1][r] = 1
                 blue_block[i-2][r] = 1
                 break
+        else:
+            blue_block[5][r] = 1
+            blue_block[4][r] = 1
 
     elif t == 3:
         for i in range(2,6):
@@ -36,22 +100,97 @@ for _ in range(n):
                 green_block[i-1][c] = 1
                 green_block[i-2][c] = 1
                 break
+        else:
+            green_block[5][c] = 1
+            green_block[4][c] = 1
         for i in range(2,6):
             if blue_block[i][r] != 0 or blue_block[i][r+1] != 0:
-                blue_block[i-1][r] = 1
-                blue_block[i-1][r+1] = 1
+                blue_block[i-1][r] = 2
+                blue_block[i-1][r+1] = 2
                 break
+        else:
+            blue_block[5][r] = 2
+            blue_block[5][r+1] = 2
+    # print("쌓는다")
+    # pprint(green_block)
+    # pprint(blue_block)
     
     #채워진 행, 열 지우고 점수올리기
-    for i in range(6):
-        if sum(green_block[i]) == 4:
-            green_block[i] = [0, 0, 0, 0]
-            point += 1
-        if sum(blue_block[i]) == 4:
-            blue_block[i] = [0, 0, 0, 0]
-            point += 1
+    combo_button = True
+    while combo_button:
+        pt = point
+        for i in range(6):
+            for j in range(4):
+                if green_block[i][j] == 0:
+                    break
+            else:
+                green_block[i] = [0, 0, 0, 0]
+                point += 1
+            for j in range(4):
+                if blue_block[i][j] == 0:
+                    break
+            else:
+                blue_block[i] = [0, 0, 0, 0]
+                point += 1
 
-    #내리기
-    
+        # print("블럭지우고점수올린다")
+        # pprint(green_block)
+        # pprint(blue_block)
         
+        #내리기
+        for row in range(5,-1,-1):
+            col = 0
+            while col < 4:
+                if green_block[row][col] == 1:
+                    g_rain_it_one(row, col)
+                elif green_block[row][col] == 2:
+                    g_rain_it_two(row, col)
+                    col += 2
+                    continue
+                col += 1
+            col = 0
+            while col < 4:
+                if blue_block[row][col] == 1:
+                    b_rain_it_one(row, col)
+                elif blue_block[row][col] == 2:
+                    b_rain_it_two(row, col)
+                    col += 2
+                    continue
+                col += 1
+        
+        if pt == point:
+            combo_button = False
+                
+        # print("블럭떨어진다")
+        # pprint(green_block)
+        # pprint(blue_block)
     
+    
+    #연한박스 부분처리
+    for row in range(2):
+        if sum(green_block[row]) != 0:
+            index = row
+            green_block = [[0, 0, 0, 0] for _ in range(2)] + green_block[row:row+4]
+            break
+    
+    for row in range(2):
+        if sum(blue_block[row]) != 0:
+            index = row
+            blue_block = [[0, 0, 0, 0] for _ in range(2)] + blue_block[row:row+4]
+            break
+    # print("블럭마무리한다")
+    # pprint(green_block)
+    # pprint(blue_block)
+    
+
+blocks = 0  
+for row in range(6):
+    for col in range(4):
+        if green_block[row][col] != 0:
+            blocks += 1
+
+        if blue_block[row][col] != 0:
+            blocks += 1
+
+print(point)
+print(blocks)
--- 백준부수기/ex.py
+++ 백준부수기/ex.py
@@ -1,18 +1,3 @@
-a = 0
-b = 1
-c = -1
+from pprint import pprint
 
-d = [a,b,c]
 
-for i in d:
-	print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!11",i)
-	if i == True:
-		print("True")
-	if i == False:
-		print("False")
-	if i == 1:
-		print(i,1)
-	if i == 0:
-		print(i,0)
-	if i == -1:
-		print(i,-1)

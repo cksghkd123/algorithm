a427bb3766d3c4aad0779e3579f2d144930b61f0
cksghkd123 <cksghkd1234@naver.com>
2021-08-26 20:22:27 +0900

    .

--- 백준부수기/16235_나무재테크_백준.py
+++ 백준부수기/16235_나무재테크_백준.py
@@ -1,63 +1,57 @@
 import collections
 
-
-def spring():
-    dead_list = []
-    breeding_list = []
-    for row, col in trees:
-        for q in range(len(trees[(row,col)])-1, -1, -1):
-            if trees[(row, col)][q] <= nutrients[row][col]:
-                nutrients[row][col] -= trees[(row,col)][q]
-                trees[(row,col)][q] +=1
-                if trees[(row,col)][q]%5 == 0:
-                    breeding_list.append((row, col))
-            else:
-                dead_list.append((row, col, trees[(row,col)][q]))
-    return dead_list, breeding_list
-
-def summer(list):
-    for row, col, x in list:
-        nutrients[row][col] += x//2
-        trees[(row,col)].remove(x)
-
 dr = [1, 1, 1, -1, -1, -1, 0, 0]
 dc = [-1, 0, 1, -1, 0, 1, 1, -1]
-def authumn(list):
-    for row, col in list:
+
+n, m, k = map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(n)]
+trees = [[collections.deque() for _ in range(n)] for _ in range(n)]
+
+b = []
+for _ in range(m):
+    x, y, z = map(int,input().split())
+    b.append((z, x-1, y-1))
+b.sort()
+
+for age, row, col in b:
+    trees[row][col].append(age)
+
+print(trees)
+nutrients = [[5 for _ in range(n)] for _ in range(n)]
+
+for _ in range(k):
+    dead_list = list()
+    breeding_list = list()
+    print(trees)
+    for i in trees:
+        rrr = len(trees[i])
+        for j in range(rrr):
+            age = trees[i].popleft()
+            if age <= nutrients[i[0]][i[1]]:
+                nutrients[i[0]][i[1]] -= age
+                age += 1
+                if age % 5 == 0:
+                    breeding_list.append((i[0], i[1]))
+                trees[i].append(age)
+            else:
+                dead_list.append((age, i[0], i[1]))
+
+    for age, row, col in dead_list:
+        nutrients[row][col] += age//2
+
+    print(breeding_list)
+    for row, col in breeding_list:
         for w in range(8):
             nr = row + dr[w]
             nc = col + dc[w]
             if 0 <= nr < n and 0 <= nc < n:
-                trees[(nr,nc)].append(1)
+                trees[(nr,nc)].appendleft(1)
 
-def winter():
     for row in range(n):
         for col in range(n):
             nutrients[row][col] += a[row][col]
 
-def count():
-    result = 0
-    for i in trees:
-        result += len(trees[i])
-    return result
-
-
-n, m, k = map(int,input().split())
-a = [list(map(int,input().split())) for _ in range(n)]
-trees = collections.defaultdict(list)
-
-for _ in range(m):
-    x, y, z = map(int,input().split())
-    trees[(x-1,y-1)].append(z)
+answer = 0
 for i in trees:
-    trees[i].sort()
-    trees[i].reverse()
-nutrients = [[5 for _ in range(n)] for _ in range(n)]
-
-for _ in range(k):
-    d_list, b_list = spring()
-    summer(d_list)
-    authumn(b_list)
-    winter()
-answer = count()
+    answer += len(trees[i])
 print(answer)

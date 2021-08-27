3d62af14162e82080bfa3d02ad6b38fae7ec7c9a
cksghkd123 <cksghkd1234@naver.com>
2021-08-27 14:10:17 +0900

    시간초과에 대하여

--- 백준부수기/16235_나무재테크_백준.py
+++ 백준부수기/16235_나무재테크_백준.py
@@ -16,42 +16,41 @@ b.sort()
 for age, row, col in b:
     trees[row][col].append(age)
 
-print(trees)
 nutrients = [[5 for _ in range(n)] for _ in range(n)]
 
 for _ in range(k):
     dead_list = list()
     breeding_list = list()
-    print(trees)
-    for i in trees:
-        rrr = len(trees[i])
-        for j in range(rrr):
-            age = trees[i].popleft()
-            if age <= nutrients[i[0]][i[1]]:
-                nutrients[i[0]][i[1]] -= age
-                age += 1
-                if age % 5 == 0:
-                    breeding_list.append((i[0], i[1]))
-                trees[i].append(age)
-            else:
-                dead_list.append((age, i[0], i[1]))
+    for row in range(n):
+        for col in range(n):
+            rrr = len(trees[row][col])
+            for _ in range(rrr):
+                age = trees[row][col].popleft()
+                if age <= nutrients[row][col]:
+                    nutrients[row][col] -= age
+                    age += 1
+                    if age % 5 == 0:
+                        breeding_list.append((row, col))
+                    trees[row][col].append(age)
+                else:
+                    dead_list.append((age, row, col))
 
     for age, row, col in dead_list:
         nutrients[row][col] += age//2
 
-    print(breeding_list)
     for row, col in breeding_list:
         for w in range(8):
             nr = row + dr[w]
             nc = col + dc[w]
             if 0 <= nr < n and 0 <= nc < n:
-                trees[(nr,nc)].appendleft(1)
+                trees[nr][nc].appendleft(1)
 
     for row in range(n):
         for col in range(n):
             nutrients[row][col] += a[row][col]
 
 answer = 0
-for i in trees:
-    answer += len(trees[i])
+for row in range(n):
+    for col in range(n):
+        answer += len(trees[row][col])
 print(answer)

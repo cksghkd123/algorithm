b9a02284229f5a302d89078ffafa04fd26d700ef
cksghkd123 <cksghkd1234@naver.com>
2021-08-28 17:03:06 +0900

    구현 이차원 배열 행열 transpose

--- 백준부수기/17140_이차원배열과연산_백준.py
+++ 백준부수기/17140_이차원배열과연산_백준.py
@@ -1,24 +1,22 @@
 import collections
 
 
-def row_calculate(leng_r, leng_c):
-    result = [[] for _ in range(leng_r)]
+def calculate(leng_r):
+    result = []
     max_leng = 0
     for row in range(leng_r):
-        count_dict = collections.defaultdict(int)
-        for col in range(leng_c):
-            if 1 <= array[row][col] <= 100:
-                count_dict[array[row][col]] += 1
-        count_list = [[v, k] for k, v in count_dict.items()]
-        count_list.sort()
-        max_leng = max(max_leng, len(count_list))
-        for i in count_list:
-            if len(result) > 100:
-                continue
-            j, k = i
-            result[row].append(k)
-            result[row].append(j)
-    
+        numbers = collections.Counter(array[row])
+        del numbers[0]
+        numbers = list(numbers.items())
+        numbers.sort(key = lambda x: (x[1],x[0]))
+        if len(numbers) > 50: numbers = numbers[:50]
+        line = []
+        for i in range(len(numbers)):
+            j, k = numbers[i]
+            line += [j,k]
+        max_leng = max(max_leng, len(numbers))
+        result.append(line)
+
     for row in range(leng_r):
         if len(result[row]) < 2*max_leng:
             zero_count = 2*max_leng-len(result[row])
@@ -27,53 +25,23 @@ def row_calculate(leng_r, leng_c):
     
     return result
 
-def col_calculate(leng_r,leng_c):
-    result = [[] for _ in range(leng_c)]
-    max_leng = 0
-    for col in range(leng_c):
-        count_dict = collections.defaultdict(int)
-        for row in range(leng_r):
-            if 1 <= array[row][col] < 100:
-                count_dict[array[row][col]] += 1
-        count_list = [[v, k] for k, v in count_dict.items()]
-        count_list.sort()
-        max_leng = max(max_leng, len(count_list))
-        for i in count_list:
-            if len(result) > 100:
-                continue
-            j, k = i
-            result[col].append(k)
-            result[col].append(j)
-
-    for col in range(leng_c):
-        if len(result[col]) < 2*max_leng:
-            zero_count = 2*max_leng-len(result[col])
-            for _ in range(zero_count):
-                result[col].append(0)
-
-    result2 = [[] for _ in range(2*max_leng)]
-    for row in range(leng_c):
-        for col in range(2*max_leng):
-            result2[col].append(result[row][col])
-    
-    return result2
-
-
 r, c, k = map(int,input().split())
 r -= 1
 c -= 1
 array = [list(map(int,input().split())) for _ in range(3)]
 time = 0
-while True:
+while time <= 100:
     leng_r = len(array)
     leng_c = len(array[0])
     if r < leng_r and c < leng_c:
         if array[r][c] == k:
             break
     if leng_r >= leng_c:
-        array = row_calculate(leng_r, leng_c)
+        array = calculate(leng_r)
     else:
-        array = col_calculate(leng_r, leng_c)
+        array = list(map(list, zip(*array)))
+        array = calculate(leng_c)
+        array = list(map(list, zip(*array)))
     time += 1
 
 if time == 100:

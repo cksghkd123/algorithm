4b7782c75e395ba73d1d342f656942b1eb75f6b2
cksghkd123 <cksghkd1234@naver.com>
2021-09-11 13:26:23 +0900

    .

--- 백준부수기/ex.py
+++ 백준부수기/ex.py
@@ -1,2 +1,13 @@
-for i in range(10):
-    print('카카오 합격'+'!'*i)
+def solution(v):
+    line_index = [(0,1,2), (0,2,1), (1,2,0)]
+    line = []
+    for n1, n2, n3 in line_index:
+        if v[n1][0] - v[n2][0] == 0:
+            if v[n3][1] == v[n2][1]:
+                return [v[n3][0], v[n3][1] + (v[n1][1] - v[n2][1])]
+            else:
+                return [v[n3][0], v[n3][1] - (v[n1][1] - v[n2][1])]
+
+
+
+print(solution([[1, 1], [2, 2], [1, 2]]))

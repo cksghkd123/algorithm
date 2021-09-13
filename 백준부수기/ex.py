e2556368a6589cdef158a35d2a826391eb9c8b55
cksghkd123 <cksghkd1234@naver.com>
2021-09-13 23:47:08 +0900

    .

--- 백준부수기/ex.py
+++ 백준부수기/ex.py
@@ -1,13 +1,68 @@
-def solution(v):
-    line_index = [(0,1,2), (0,2,1), (1,2,0)]
-    line = []
-    for n1, n2, n3 in line_index:
-        if v[n1][0] - v[n2][0] == 0:
-            if v[n3][1] == v[n2][1]:
-                return [v[n3][0], v[n3][1] + (v[n1][1] - v[n2][1])]
-            else:
-                return [v[n3][0], v[n3][1] - (v[n1][1] - v[n2][1])]
+def combination(left_arrow, index, info, result):
+    global max_score
+    global win_list
 
+    if index == len(info):
+        score = 0
 
+        for i in range(11):
+            if i in result:
+                score += (10-i)
+            elif info[i] != 0:
+                score -= (10-i)
+        if score > 0:
+            print(score)
+        if max_score > score:
+            return
+        else:
+            max_score = score
+            win_list = result
+            return
+    
+    if left_arrow == 0:
+        score = 0
+        for i in range(11):
+            if i in result:
+                score += (10-i)
+            elif info[i] != 0:
+                score -= (10-i)
+        if score >0:
+            print(score)
+        if max_score > score:
+            return
+        else:
+            max_score = score
+            win_list = result
+            return
+    
+    if info[index] < left_arrow:
+        combination(left_arrow - info[index]-1, index+1, info, result+[index])
+        combination(left_arrow, index+1, info, result)
+    else:
+        combination(left_arrow, index+1, info, result)
 
-print(solution([[1, 1], [2, 2], [1, 2]]))
+max_score = 0
+win_list = []
+
+def solution(n, info):
+    combination(n, 0, info, [])
+
+    if max_score <= 0:
+        return [-1]
+
+    answer = [0 for _ in range(11)]
+    left_arrow = n
+
+    for index in win_list:
+        answer[index] += info[index]+1
+        left_arrow -= (info[index] + 1)
+        print(left_arrow)
+
+    if left_arrow > 0:
+        answer[10] += left_arrow
+        
+    print(max_score)
+    print(win_list)
+    return answer
+
+print(solution(2,[1,0,0,0,0,0,0,0,0,0,0]))
--- 백준부수기/ex2.py
+++ 백준부수기/ex2.py
@@ -1,53 +1,29 @@
-def candidate_key(relation, candidate):
-    target = len(relation)
-    temp = []
-    for i in range(target):
-        temp.append(tuple(map(lambda n:relation[i][n], candidate)))
-    temp = set(temp)
+def solution(board, skill):
+    answer = 0
+    temp = [[0 for _ in range(len(board[0])+1)] for _ in range(len(board))]
 
-    if len(temp) == target:
-        global answer
-        answer += 1
-        global button
-        button = True
+    for i in range(len(skill)):
+        t, r1, c1, r2, c2, degree = skill[i]
 
-def combination(relation, temp, k, target, result, buttons):
-    if len(result) == target:
-        candidate_key(relation, result)
-        return
+        if t == 1:
+            degree = -degree
 
-    if k == len(temp):
-        return
+        for r in range(r1, r2+1):
+            temp[r][c1] += degree
+        for r in range(r1, r2+1):
+            temp[r][c2+1] += (-degree)
     
-    if buttons[temp[k]] == True:
-        combination(relation, temp, k+1, target, result, buttons)
-    else:
-        combination(relation, temp, k+1, target, result + [temp[k]], buttons)
-        combination(relation, temp, k+1, target, result, buttons)
+    answer = 0
 
-answer = 0
-button = False
-def solution(relation):
-    global button
-    index_info = [i for i in range(len(relation[0]))]
-
-    buttons = [False for _ in range(len(relation[0]))]
-
-    for r in range(1,len(index_info)+1):
-        for i in range(len(index_info)):
-            button = False
-            if buttons[i] == True:
-                continue
-            temp = index_info[i:]
-            combination(relation, temp, 1, r, [temp[0]], buttons)
-            if button == True:
-                buttons[i] = True
-            print(r, i, buttons)
-
+    for row in range(len(board)):
+        weigh = 0
+        for col in range(len(board[0])):
+            weigh += temp[row][col]
+            if board[row][col] + weigh > 0:
+                answer += 1
+        
     return answer
 
 
 
-
-
-print(solution([['a',1,'aaa','c','ng'],['b',1,'bbb','c','g'],['c',1,'aaa','d','ng'],['d',2,'bbb','d','ng']]))
+print(solution([[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]],[[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]]))

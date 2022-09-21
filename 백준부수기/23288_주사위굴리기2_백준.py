485eb57c3696e42016e0ffc0212ada4ca37297a7
cksghkd123 <cksghkd1234@naver.com>
2022-09-21 18:40:15 +0900

    구현

--- 백준부수기/23288_주사위굴리기2_백준.py
+++ 백준부수기/23288_주사위굴리기2_백준.py
@@ -1,4 +1,5 @@
 import collections
+from pprint import pprint
 
 
 n, m, k = map(int,input().split())
@@ -7,6 +8,7 @@ for _ in range(n):
     map_list.append(list(map(int,input().split())))
 
 
+#BxC 에서 C를 score_guide에 미리 저장시켜준다.
 map_trans = ((0,1),(1,0),(0,-1),(-1,0)) #우,하,좌,상
 score_guide = [[0 for _ in range(m)] for _ in range(n)]
 visited = [[False for _ in range(m)] for _ in range(n)]
@@ -41,10 +43,11 @@ for row in range(n):
             score_guide[x][y] = answer
 
 
+#구현
 dice_trans = [(3,2,0,1,4,5),(5,4,2,3,0,1),(2,3,1,0,4,5),(4,5,2,3,1,0)]
 
 d = 0
-dice = [1,6,3,4,2,5] # 위,아래,동,서,남,북
+dice = [1,6,3,4,5,2] # 위,아래,동,서,남,북
 score = 0
 row = 0
 col = 0

c47d1b95913882b6cbbe8820d910570cb4eb11b4
cksghkd123 <cksghkd1234@naver.com>
2024-07-11 16:03:20 +0900

    LIS dp

---
+++ 백준부수기/2565_전깃줄_백준.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+line_list = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [1 for _ in range(n)]
+
+line_list.sort()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if line_list[j][1] < line_list[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+
+print(n-dp[-1])
+

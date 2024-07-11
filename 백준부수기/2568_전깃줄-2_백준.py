871db20dd95cbe4d373425b8c3f56d34fa76d191
cksghkd123 <cksghkd1234@naver.com>
2024-07-11 15:19:59 +0900

    어려움

---
+++ 백준부수기/2568_전깃줄-2_백준.py
@@ -0,0 +1,59 @@
+def bisect_left(input_list, target):
+    left = 0
+    right = len(input_list)
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if input_list[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    
+    return left
+
+
+n = int(input())
+
+line_list = [list(map(int, input().split())) for _ in range(n)]
+
+LIS = []
+pos = [0] * n
+prev = [-1] * n
+
+for i in range(n):
+    if not LIS or LIS[-1] < line_list[i][1]:
+        if LIS:
+            prev[i] = pos[len(LIS) - 1]
+        LIS.append(line_list[i][1])
+        pos[len(LIS) - 1] = i    
+    else:
+        idx = bisect_left(LIS, line_list[i][1])
+        LIS[idx] = line_list[i][1]
+        pos[idx] = i
+        if idx != 0:
+            prev[i] = pos[idx - 1]
+
+result = []
+current = pos[len(LIS) - 1]
+
+while current != -1:
+    result.append(line_list[current][0])
+    current = prev[current]
+
+result.reverse()
+
+print(len(result))
+print(*result)
+
+
+
+# 8
+# 1 8
+# 3 9
+# 2 2
+# 4 1
+# 6 4
+# 10 10
+# 9 7
+# 7 6

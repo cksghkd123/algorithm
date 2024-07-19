4b1ca32d80270cfb11a168f3e8cd4b08783637bd
cksghkd123 <cksghkd1234@naver.com>
2024-07-19 14:23:19 +0900

    이분탐색

--- 백준부수기/5639_이진검색트리_백준.py
+++ 백준부수기/5639_이진검색트리_백준.py
@@ -1,30 +1,37 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.key = key
+import sys
+sys.setrecursionlimit(10**6)
 
-def insert(node, key):
-    if node is None:
-        return Node(key)
-    if key < node.key:
-        node.left = insert(node.left, key)
-    else:
-        node.right = insert(node.right, key)
-    return node
+def lower_bound_binary_search(arr, n):
+    left = 0
+    right = len(arr)
 
-def postorder(node):
-    if node:
-        postorder(node.left)
-        postorder(node.right)
-        print(node.key)
+    while left < right:
+        mid = (left+right) // 2
+        
+        if arr[mid] < n:
+            left = mid + 1
+        else:
+            right = mid
+    return left
 
-root = None
+input_list = []
+
 while True:
     try:
         input_value = int(input())
-        root = insert(root, input_value)
+        input_list.append(input_value)
     except:
         break
+
+def pre_to_post_order(preorder_list):
+    if len(preorder_list) == 0:
+        return
     
-postorder(root)
+    i = lower_bound_binary_search(preorder_list[1:], preorder_list[0])
+    pre_to_post_order(preorder_list[1:1+i])
+    pre_to_post_order(preorder_list[1+i:])
+    
+    print(preorder_list[0])
+
+
+pre_to_post_order(input_list)

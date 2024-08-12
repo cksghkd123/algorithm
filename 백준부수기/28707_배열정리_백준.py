2c45c4788b9941898aaa4d71787c6854be948878
cksghkd123 <cksghkd1234@naver.com>
2024-08-12 18:21:22 +0900

    다익스트라

--- 백준부수기/28707_배열정리_백준.py
+++ 백준부수기/28707_배열정리_백준.py
@@ -1,37 +1,37 @@
-import collections
 import heapq
-
 
 N = int(input())
 A = list(map(int, input().split()))
 M = int(input())
 commands = [list(map(int, input().split())) for _ in range(M)]
 
+target = tuple(sorted(A))
+
 visited = set()
 
-target = ''.join(map(str, sorted(A)))
-
 heap = []
-heapq.heappush(heap,(0,''.join(map(str, A))))
+heapq.heappush(heap, (0, tuple(A)))
 
-answer = 0
+answer = -1
 
 while heap:
-    print(heap)
     count, curr = heapq.heappop(heap)
-    if curr not in visited:
-        visited.add(curr)
-    else:
+
+    if curr in visited:
         continue
+
+    visited.add(curr)
 
     if curr == target:
         answer = count
         break
 
-    for c in commands:
-        nxt = curr
-        print(type(nxt))
-        nxt[c[0]-1], nxt[c[1]-1] = nxt[c[1]-1], nxt[c[0]-1]
-        heapq.heappush(heap, (count+c[2], nxt))
+    for l, r, c in commands:
+        l -= 1
+        r -= 1
+        nxt = list(curr)
+        nxt[l], nxt[r] = nxt[r], nxt[l]
+
+        heapq.heappush(heap, (count + c, tuple(nxt)))
 
 print(answer)
